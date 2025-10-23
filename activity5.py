9a3c91cbc0bf85e775901c11858473ee56dc7413
Vaz4796 <vazq4796@bears.unco.edu>
2025-10-23 23:55:06 +0000

    first draft

---
+++ activity5.py
@@ -0,0 +1,11 @@
+def more_than_20(file):
+    wordls_20 = []
+
+    with open(file, 'r') as worlds_file:
+
+        for world in worlds_file:
+
+            worlds = world.strip()
+
+
+    return wordls_20
